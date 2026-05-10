e517586146f8464def41a057d5927e5dd544f01d
Nờ Lê Lộc <141159425+nleloc@users.noreply.github.com>
2026-05-10 02:53:05 +0000

    refactor: change how modifyInFO.sh work, version naming
    change how realme rom version naming will be handled
    test modify 'ro.build.version.oplusrom.display' for A16 too
    de-hardcode 'NT Mods' in modifyINFO.sh, move to Version file for more flexiable (thực ra để dễ đè tem hơn xD)

--- bin/ddevice/modifyINFO.sh
+++ bin/ddevice/modifyINFO.sh
@@ -10,17 +10,10 @@ DEFAULT_VALUE=$(get_prop ro.build.version.oplusrom.display)
 BRAND=$(cat $work_dir/bin/ddevice/brand_os.txt)
 ANDROID_VER=$(cat $work_dir/bin/ddevice/androidver.txt)
 
-if [[ $ANDROID_VER == "15" || $ANDROID_VER == "14" ]]; then
-
-change_prop ro.build.version.oplusrom.display "$DEFAULT_VALUE | NTMods $NOTHING_VERSION"
-
-if [[ $BRAND == "RealmeUI" ]]; then
-  DEFAULT_VALUE1=$(get_prop ro.build.version.realmeui)
-  change_prop ro.build.version.oplusrom.display "$DEFAULT_VALUE | NTMods $NOTHING_VERSION"
-fi
-
+# Try to change information for A16 too
+if [[ $BRAND != "RealmeUI" ]]; then
+  change_prop ro.build.version.oplusrom.display "$DEFAULT_VALUE | $NOTHING_VERSION"
 else
-
-echo "[INFO] - Skip change information for A16"
-
+  DEFAULT_VALUE_RM=$(get_prop ro.build.version.realmeui)
+  change_prop ro.build.version.oplusrom.display "$DEFAULT_VALUE_RM | $NOTHING_VERSION"
 fi
